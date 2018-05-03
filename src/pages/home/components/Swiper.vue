7c6e1fc4e4a7bede4b6e624a6f5cb165e71ec0cb
xls <15110069372@163.com>
2018-05-03 15:51:55 +0800

    chenge

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide><img class="swiper-img" src="@/assets/images/bg.jpg" alt=""></swiper-slide>
+      <swiper-slide><img class="swiper-img" src="@/assets/images/qw.jpg" alt=""></swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
