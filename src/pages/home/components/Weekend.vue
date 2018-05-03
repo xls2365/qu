fa54b1cd418f9f4134f41ec7cdb93e5a5e6e86e6
xls <15110069372@163.com>
2018-05-03 21:14:10 +0800

    add recommend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id">
+          <div class="item-img-wrapper">
+            <img class="item-img" :src="item.imgUrl" />
+          </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
